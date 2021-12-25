43c1b544a500fe07011a665ac3e15cbf06ab2a94
DaveIshaSnehalbhai <96402991+DaveIshaSnehalbhai@users.noreply.github.com>
2021-12-25 12:02:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 model = joblib.load('Iris Data(1)')  
 st.title('Iris Flower Data')  
 
-ip = st.slider('Select Stepal Length',min_value=0.0,max_value=10.0,step=0.1)        
+ip = st.slider('Select Stepal Length',min_value=0.0,max_value=10.0,step=0.1)           
 jp = st.slider('Select Stepal Width',min_value=0.0,max_value=10.0,step=0.1)  
 kp = st.slider('Select Petal Length',min_value=0.0,max_value=10.0,step=0.1)    
 lp = st.slider('Select Petal Width',min_value=0.0,max_value=10.0,step=0.1)    
