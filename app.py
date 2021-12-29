3b8ac6787b9eec6ea2a4fa9c8470682e06c38152
DaveIshaSnehalbhai <96402991+DaveIshaSnehalbhai@users.noreply.github.com>
2021-12-29 09:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Iris Flower Data')
 
 ip = st.slider('Select Stepal Length',min_value=0.0,max_value=10.0,step=0.1)
-jp = st.slider('Select Stepal Width',min_value=0.0,max_value=10.0,step=0.1)
+jp = st.slider('Select Stepal Width',min_value=0.0,max_value=10.0,step=0.1)  
 kp = st.slider('Select Petal Length',min_value=0.0,max_value=10.0,step=0.1)
 lp = st.slider('Select Petal Width',min_value=0.0,max_value=10.0,step=0.1)
 
@@ -17,5 +17,5 @@ model.fit(iris.data,iris.target)
 op = model.predict([[ip,jp,kp,lp]])
 op = iris.target_names[op[0]]
 
-st.title(op[0])
+st.title(op[0])  
 
